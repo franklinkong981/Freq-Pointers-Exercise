9e8fcba3e909f456dff143e83c060c4a13d2b175
Franklin Kong <franklinkong981@gmail.com>
2025-06-09 11:47:22 -0700

    Completed average pair problem

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,18 @@
 // add whatever parameters you deem necessary
-function averagePair() {
+function averagePair(arr, averageValue) {
+  let leftIndex = 0;
+  let rightIndex = arr.length - 1;
+
+  while (leftIndex < rightIndex) {
+    let average = arr[leftIndex] + arr[rightIndex] / 2;
+    if (average == averageValue) {
+      return true;
+    } else if(average < averageValue) {
+      leftIndex++;
+    } else {
+      rightIndex--;
+    }
+  }
+
+  return false;
 }
