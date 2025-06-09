8847ff805ad4a5c4d2707ce4a39a2ea10b1b8997
Franklin Kong <franklinkong981@gmail.com>
2025-06-09 12:50:35 -0700

    Completed separatePositive problem

--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,21 @@
 // add whatever parameters you deem necessary
-function separatePositive() {
+function separatePositive(arr) {
+  let headPointer = 0;
+  let tailPointer = 0;
+
+  while (headPointer < arr.length) {
+    if (arr[tailPointer] > 0) {
+      headPointer++;
+      tailPointer++;
+    } else if (arr[headPointer] < 0) {
+      headPointer++;
+    } else {
+      //tailPointer points to negative number, headPointer points to positive number, time to swap.
+      [arr[tailPointer], arr[headPointer]] = [arr[headPointer], arr[tailPointer]];
+      headPointer++;
+      tailPointer++;
+    }
+  }
+
+  return arr;
 }
