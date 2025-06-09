f7ac7b61a72254c3389eea9ad473e543bbf29ba5
Franklin Kong <franklinkong981@gmail.com>
2025-06-09 12:28:37 -0700

    Solved twoArrayObject problem

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,12 @@
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+function twoArrayObject(keyArray, valueArray) {
+  let arrayObject = {};
+  for (let keyIndex = 0; keyIndex < keyArray.length; keyIndex++) {
+    if (keyIndex < valueArray.length) {
+      arrayObject[keyArray[keyIndex]] = valueArray[keyIndex];
+    } else {
+      arrayObject[keyArray[keyIndex]] = null;
+    }
+  }
+  return arrayObject;
 }
