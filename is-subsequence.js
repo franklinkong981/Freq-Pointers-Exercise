083af0bd0dd814d16b70d282ae07eef0396f0f25
Franklin Kong <franklinkong981@gmail.com>
2025-06-10 11:44:35 -0700

    Solved isSubsequence problem

--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,14 @@
 // add whatever parameters you deem necessary
-function isSubsequence() {
+function isSubsequence(substring, fullString) {
+  let substringPointer = 0;
+  let fullStringPointer = 0;
+
+  while (substringPointer < substring.length && fullStringPointer < fullString.length) {
+    if (substring[substringPointer] == fullString[fullStringPointer]) {
+      substringPointer++;
+    }
+    fullStringPointer++;
+  }
+
+  return substringPointer == substring.length;
 }
