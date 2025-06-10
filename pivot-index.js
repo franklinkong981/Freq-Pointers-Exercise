a665b75d167c9dfce4f459038de49c390ad5cf5b
Franklin Kong <franklinkong981@gmail.com>
2025-06-10 13:04:32 -0700

    Completed exercise with pivotIndex problem

--- pivot-index.js
+++ pivot-index.js
@@ -1,3 +1,26 @@
 // add whatever parameters you deem necessary
-function pivotIndex() {
+function pivotIndex(nums) {
+  if (nums.length == 0) return false;
+  if (nums.length == 1) return true;
+
+  let leftSum = 0;
+  let rightSum = 0;
+
+  //We assume pivot index could be first value (leftSum = 0) or last value (rightSum = 0)
+  let pivotIndex = 0;
+  //calculate rightSum first, aka sum from second value to end of array.
+  for (let i = 1; i < nums.length; i++) {
+    rightSum += nums[i];
+  }
+
+  if (leftSum == rightSum) return 0;
+
+  while (pivotIndex < nums.length - 1) {
+    leftSum += nums[pivotIndex];
+    pivotIndex++;
+    rightSum -= nums[pivotIndex];
+    if (leftSum == rightSum) return pivotIndex;
+  }
+
+  return -1;
 }
