3ef75351fd2eb35d86fa7d647f056717445ace20
Franklin Kong <franklinkong981@gmail.com>
2025-06-10 12:34:53 -0700

    Solved Further Study problem longestFall

--- longest-fall.js
+++ longest-fall.js
@@ -1,3 +1,19 @@
 // add whatever parameters you deem necessary
-function longestFall() {
+function longestFall(nums) {
+  let longestFall = 1;
+  let currentFall = 1;
+
+  if (nums.length == 0) return 0;
+  if (nums.length == 1) return 1;
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] < nums[i-1]) {
+      currentFall++;
+    } else {
+      longestFall = Math.max(longestFall, currentFall);
+      currentFall = 1;
+    }
+  }
+
+  return Math.max(longestFall, currentFall);
 }
