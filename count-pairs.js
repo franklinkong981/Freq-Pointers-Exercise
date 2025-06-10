3594bb01241bb5461158fdc5ef22e98eb8c0292b
Franklin Kong <franklinkong981@gmail.com>
2025-06-10 12:03:19 -0700

    Finished base requirements by solving numPairs problem

--- count-pairs.js
+++ count-pairs.js
@@ -1,3 +1,17 @@
 // add whatever parameters you deem necessary
-function countPairs() {
+function countPairs(arr, targetSum) {
+  let numberFrequencies = {};
+  let numPairs = 0;
+
+  for (let num of arr) {
+    let otherNum = targetSum - num;
+    if (numberFrequencies[otherNum] && numberFrequencies[otherNum] > 0) {
+      numberFrequencies[otherNum]--;
+      numPairs++;
+    } else {
+      numberFrequencies[num] = (numberFrequencies[num] + 1) || 1;
+    }
+  }
+
+  return numPairs;
 }
